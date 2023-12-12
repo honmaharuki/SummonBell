85f870991db1e893b69219db646fda61445725b5
honnnoaida <honnnoaida@gmail.com>
2023-12-12 21:55:39 +0900

    チャット機能基礎追加

--- lib/screens/top.dart
+++ lib/screens/top.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:uuid/uuid.dart';
 
 import 'package:summon_bell/models/top_model.dart';
-
 import 'package:summon_bell/screens/char_add.dart';
 import 'package:summon_bell/screens/char_select.dart';
 import 'package:summon_bell/screens/general_settings.dart';
 import 'package:summon_bell/screens/open_ai_settings.dart';
 
-// TopModel の Provider
-final topModelProvider = Provider((ref) => TopModel(aId: 1));
+// TopModel's Provider
+final topModelProvider = Provider(
+    (ref) => TopModel(aId: 1)); // Replace TopModel with your actual model
 
 class TopWidget extends HookConsumerWidget {
   const TopWidget({Key? key}) : super(key: key);
@@ -18,91 +21,136 @@ class TopWidget extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final _model = ref.watch(topModelProvider);
-
-    Widget _buildListTile(String title, Widget Function() buildRoute) {
-      return ListTile(
-        title: Text(title),
-        onTap: () {
-          Navigator.pop(context);
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => buildRoute()));
-        },
-      );
-    }
+    final messages = useState<List<types.Message>>([]);
 
     return Scaffold(
-      backgroundColor: Color(0xFFEAEDF0),
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("title"),
+      backgroundColor: const Color(0xFFEAEDF0),
+      appBar: _buildAppBar(context),
+      body: _buildBody(context, messages),
+      drawer: _buildDrawer(context),
+    );
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: const Text("Title"),
+      leading: _buildDrawerButton(context),
+      actions: _buildAppBarActions(),
+    );
+  }
+
+  IconButton _buildDrawerButton(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.menu),
+      onPressed: () => Scaffold.of(context).openDrawer(),
+      tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+    );
+  }
+
+  List<Widget> _buildAppBarActions() {
+    return [
+      IconButton(
+        icon: const Icon(Icons.settings),
+        tooltip: 'Settings',
+        onPressed: () {
+          // Settings button logic here
+        },
       ),
-      body: SafeArea(
-        top: true,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            // 他のウィジェット部分は省略
-            Row(
+      IconButton(
+        icon: const Icon(Icons.door_sliding_sharp),
+        tooltip: 'Search',
+        onPressed: () {
+          // Search button logic here
+        },
+      ),
+    ];
+  }
+
+  SafeArea _buildBody(
+      BuildContext context, ValueNotifier<List<types.Message>> messages) {
+    return SafeArea(
+      top: true,
+      child: SingleChildScrollView(
+        child: Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 600),
+            child: Column(
               mainAxisSize: MainAxisSize.max,
               children: [
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(8, 0, 8, 0),
-                    child: TextFormField(
-                      controller: _model.textController,
-                      focusNode: _model.textFieldFocusNode,
-                      autofocus: true,
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        labelText: 'Label here...',
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.grey,
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print('Button pressed ...');
-                  },
-                  child: Text('Button'),
-                  style: ElevatedButton.styleFrom(
-                    primary: Theme.of(context).primaryColor,
-                    onPrimary: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                ),
+                _buildImageHeader(context),
+                _buildChatSection(messages),
               ],
             ),
-            // 他のウィジェット部分は省略
-          ],
+          ),
         ),
       ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            _buildListTile('Char Add', () => CharAddWidget()),
-            _buildListTile('Char Select', () => CharSelectWidget()),
-            _buildListTile('General Settings', () => GeneralSettingsWidget()),
-            _buildListTile('OpenAI Settings', () => OpenAISettingsWidget()),
-          ],
-        ),
+    );
+  }
+
+  Padding _buildImageHeader(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 4.0),
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          double width = MediaQuery.of(context).size.width;
+          double imageWidth = width - 16;
+          return Image.asset(
+            'assets/images/sura.png',
+            width: imageWidth,
+            fit: BoxFit.fitWidth,
+          );
+        },
       ),
+    );
+  }
+
+  Container _buildChatSection(ValueNotifier<List<types.Message>> messages) {
+    return Container(
+      height: 400,
+      child: Chat(
+        messages: messages.value,
+        onSendPressed: (message) => _handleSendPressed(message, messages),
+        user: const types.User(id: 'user_id'),
+      ),
+    );
+  }
+
+  void _handleSendPressed(
+      types.PartialText message, ValueNotifier<List<types.Message>> messages) {
+    final textMessage = types.TextMessage(
+      author: const types.User(id: 'user_id'),
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: const Uuid().v4(),
+      text: message.text,
+    );
+
+    messages.value = [...messages.value, textMessage];
+    // Implement logic to send message to server
+  }
+
+  Drawer _buildDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          _buildDrawerItem(context, 'Char Add', CharAddWidget()),
+          _buildDrawerItem(context, 'Char Select', CharSelectWidget()),
+          _buildDrawerItem(
+              context, 'General Settings', GeneralSettingsWidget()),
+          _buildDrawerItem(context, 'OpenAI Settings', OpenAISettingsWidget()),
+        ],
+      ),
+    );
+  }
+
+  ListTile _buildDrawerItem(BuildContext context, String title, Widget widget) {
+    return ListTile(
+      title: Text(title),
+      onTap: () {
+        Navigator.pop(context);
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => widget));
+      },
     );
   }
 }
